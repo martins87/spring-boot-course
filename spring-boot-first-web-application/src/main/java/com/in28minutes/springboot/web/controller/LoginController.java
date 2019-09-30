ad057e4209a9fadd890707044aaa78bc843b6e07
Daniel Martins <martinsda@gmail.com>
2019-09-30 11:57:48 -0300

    Edit login page

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.in28minutes.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,7 +11,11 @@ public class LoginController {
 
 	@RequestMapping("/login")
 //	@ResponseBody
-	public String loginMessage() {
+	public String loginMessage(@RequestParam String name, ModelMap model) {
+		// makes the model available to the view page
+		model.put("name", name);
+		
+		System.out.println("name: " + name);
 		return "login";
 	}
 }
--- spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/login.jsp
+++ spring-boot-first-web-application/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
 		<title>First Web Application</title>
 	</head>
 	<body>
-		My First JSP!
+		My First JSP! Welcome, ${name}!
 	</body>
 </html>
